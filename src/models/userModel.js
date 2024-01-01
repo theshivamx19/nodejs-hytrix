ef950a8e5afc8ce89832e99031019effd61fac18
theshivamx19 <shivammaurya234@gmail.com>
2024-01-01 23:16:13 +0530

    start new view panel in nodejs

---
+++ src/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+    mobile : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    address : {
+        type : String,
+        required : true, 
+        trim : true
+    }
+}, {timestamps : true})
+
+const user = mongoose.model("User", userSchema)
+module.exports = user
