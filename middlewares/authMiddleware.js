3ce439c8ca39f32c320e9edf0c41e2d41e0fed14
theshivamx19 <shivammaurya234@gmail.com>
2024-01-10 16:27:30 +0530

    authentication status code updated

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,22 @@
 import jwt from 'jsonwebtoken'
 
 const authentication = async (req, res, next) => {
-    const token = req.cookies['x-api-key']
-    if (!token) {
-        return res.status(400).send({ status: false, message: "Token must be present" })
-    }
-    await jwt.verify(token, 'secretKey', (err, decodedToken) => {
-        if (err) {
-            return res.status(501).send({ status: false, message: "User is unauthenticated" })
+    try {
+        const token = req.cookies['x-api-key']
+        if (!token) {
+            return res.status(400).send({ status: false, message: "Token must be present" })
         }
-        req.tokenUserId = decodedToken.userId
-        next()
-    })
+        await jwt.verify(token, 'secretKey', (err, decodedToken) => {
+            if (err) {
+                return res.status(401).send({ status: false, message: "User is unauthenticated" })
+            }
+            req.tokenUserId = decodedToken.userId
+            next()
+        })
+    }
+    catch (err) {
+        return res.status(500).send({status : false, message : err.message})
+    }
 }
 
 export default { authentication }
