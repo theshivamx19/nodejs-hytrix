fb5035f80bdd7ba52bf54fd792d31fa8f1e504ab
Pradeep Kumar Maurya <88256058+pradeep4j@users.noreply.github.com>
2024-03-14 16:12:10 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,104 +1,110 @@
 import express from "express";
-import cors from 'cors'
-import { isAdmin, protectRoute } from '../middlewares/authMiddleware.js';
+import { isAdmin,protectRoute } from '../middlewares/authMiddleware.js';
 import axios from 'axios'; //here axios only used to validate reCaptcha
-import { login, logout, catCreate, catGettting, catEditById, createCompliances, complianceGetting, userCreate, userGetting, stateCreate, checkListCreate, checkListGetting, checkListFilter, checkListFind, createBranch, branchGetting, createNotification, notificationGetting, addlist, checkData, getCheckData, updateCompliancesById, complianceFilter, complianceRejectedFilter, checkListRejectedFilter, checkListCreateFilter, checkListApproveFilter, checkListAllFilter, createElibrary, elibraryGetting, elibraryApproved, elibraryRejected, checklistOnCreateegetting, checklistApprovegetting, checklistOnRejectegetting, gettingCompliances, complianceApproveFilter, gettingAuditor, gettingChecklist, createAudit, auditGetting, updateAudit, createLiseReg, liseRegGetting, liseRegUpdateById, liseRegHistoryFilter, elibraryGettingById, auditFilter, createCompanyRegistration, createCompanyClientContact } from '../controllers/Admin.js';
-
+import {login,logout,auditoreGet,createAudit,catCreate,catGettting,catEditById,deleteCat,stateGetting,gettingUser,notificationCreate,createUser,editUser,deleteUser,gettingCompany,gettingCompanyTable,gettingBranch,gettingCompliances,gettingCompliancesAll,createBranch,createCompliances,gettingNotification,gettingCompliancesById,gettingCompliancesOnCreate,updateCompliancesById,complianceApporve,gettingCompliancesReject,complianceReject,complianceFilter,complianceRejectedFilter,gettingCompliancesAllFilter,checkListCreate,checkListFind,updateChecklistsById,checklistOnCreateegetting,gettingchecklistById,checklistAllgetting,checklistApporve,checkListAllFilter,checkListCreateFilter,checklistApprovegetting,checkListApproveFilter,checklistOnRejectegetting,rejectChecklist,checkListRejectedFilter,gettingchecklistAllCompliance,complianceApproveFilter,gettingAuditDetail,createLiseReg,liseRegGetting,liseRegUpdateById,liseRegHistoryFilter,regsApporve,liseRegGettingById,auditChecklistFilter,auditchecklistGetonCreate,auditFilter,gettingOnGoingAuditDetail,gettingAuditorOverdueDashboard,elibraryCreate,createCompany} from '../controllers/Admin.js';//elibraryGet ,createRegistrationTab2,createRegistrationTab3,createRegistrationTab4,createRegistrationTab5,createRegistrationTab6,createRegistrationTab7
 import { upload } from "../middlewares/multerConfig.js";
-// import multer from "multer";
-// import path from "path";
 const router = express.Router();
+router.post('/login',login); 
+router.get('/logout',logout);
+/*audit start*/
+router.post('/auditCreate',protectRoute, createAudit)
+router.get('/auditoreGet',protectRoute,auditoreGet);
+router.get('/gettingOnGoingAuditDetail',protectRoute, gettingOnGoingAuditDetail)
+router.get('/gettingOverviewAuditDetail',protectRoute, gettingAuditorOverdueDashboard)
+router.get('/gettingAuditDetail',protectRoute, gettingAuditDetail)
+router.post('/auditChecklistFilter',protectRoute,auditChecklistFilter)
+router.get('/auditchecklistGetonCreate',protectRoute,auditchecklistGetonCreate)
+// router.post('/auditFilter',protectRoute, auditFilter);
+router.post('/auditAllFilter',protectRoute, auditFilter);
+// router.post('/executiveGet',protectRoute, executiveGet)
 
-router.post('/login', login);
-router.get('/logout', logout);
-router.post('/catCreate', catCreate);
-router.get('/catGettting', catGettting);
-// router.post('/add-user',createUsers); //router.route('/add-user').post(protectRoute,createUsers); both the ways will work
-// router.route('/user-profile/:id').get(protectRoute,usersProfileById);  //this way of request routes is also be taken 
-// router.put('/update-user-profile/:id',protectRoute,updateUsersProfileById);
-router.put('/catEditById/:id', protectRoute, catEditById);
-// router.route('/allUsers/:id').get(protectRoute,isAdmin,allUsers);
-// router.post('/searchUsersRecords/:id',protectRoute,isAdmin,searchUsersRecordsNav);
-// router.get('/confirmuser/:token',confirmuser);       
-// router.delete('/delete/:id',deleteUsers);
-// const storage = multer.memoryStorage();
+/*audit ends*/
+/**Elibrary start */
+router.post('/elibraryCreate',upload.single("image"),protectRoute, elibraryCreate)
+// router.get('/elibraryGet',protectRoute, elibraryGet)
+/**Elibrary end */
+/*category start*/
+router.post('/catCreate',protectRoute,catCreate);
+router.get('/catGettting',protectRoute,catGettting);
+router.put('/catEditById/:id',protectRoute,catEditById);
+router.delete('/deleteCat/:id',protectRoute,deleteCat);
+/*category ends*/
+router.get('/stateGetting',protectRoute,stateGetting);
+router.post('/notificationCreate', upload.single("document"),protectRoute,notificationCreate);
+router.get('/gettingNotification',protectRoute, gettingNotification);
+router.get('/comapany/:comany/:state/:date',protectRoute,notificationCreate);
+/*users start*/
+router.post('/userCreate',protectRoute, createUser )
+router.get('/gettingUser',protectRoute,gettingUser);
+router.put('/editUser/:id',protectRoute,editUser);
+router.delete('/deleteUser/:id',protectRoute,deleteUser);
+/*users ends*/
+/*branch start*/
+router.get('/gettingBranch',protectRoute, gettingBranch);
+router.post('/createBranch',protectRoute, createBranch);
+/*branh ends*/
+/*compliance start*/
+router.post('/createCompliances',upload.fields([{ name: 'image' }, { name: 'document' }]),protectRoute, createCompliances);
+router.get('/gettingCompliances',protectRoute, gettingCompliances);
+router.get('/gettingCompliancesAll',protectRoute, gettingCompliancesAll);
+router.get('/gettingCompliancesOnCreate',protectRoute, gettingCompliancesOnCreate);
+router.get('/gettingCompliancesById/:id',protectRoute,gettingCompliancesById);
+router.put('/updateCompliancesById/:id',upload.fields([{ name: 'image' }, { name: 'document' }]),protectRoute,updateCompliancesById);
+router.patch('/complianceApporve',protectRoute,complianceApporve);
+router.patch('/complianceReject',protectRoute,complianceReject);
+router.post('/complianceApproveFilter', protectRoute,complianceApproveFilter );
+router.post('/gettingCompliancesFilter', protectRoute,complianceFilter );
+router.post('/gettingCompliancesAllFilter', protectRoute,gettingCompliancesAllFilter);
+router.post('/gettingCompliancesRejetFilter', protectRoute,complianceRejectedFilter);
+router.get('/gettingCompliancesReject',protectRoute, gettingCompliancesReject);
+/*compliance ends*/
+/*checklist start*/
+router.post('/checkListCreate', upload.fields([{ name: 'image' }, { name: 'document' }]) ,protectRoute, checkListCreate)
+router.get('/checkListGetting',protectRoute, checkListFind);
+router.put('/updateChecklistsById/:id',upload.fields([{ name: 'image' }, { name: 'document' }]),protectRoute,updateChecklistsById);
+router.get('/checklistOnCreateegetting',protectRoute, checklistOnCreateegetting);
+router.get('/gettingchecklistById/:id',protectRoute,gettingchecklistById);
+router.get('/checklistAllgetting',protectRoute, checklistAllgetting);
+router.get('/checklistApprovegetting',protectRoute, checklistApprovegetting)
+router.patch('/checklistApporve',protectRoute,checklistApporve);
+router.post('/checkListAllFilter',protectRoute, checkListAllFilter)
+router.post('/checkListApproveFilter',protectRoute, checkListApproveFilter)
+router.post('/checkListCreateFilter',protectRoute, checkListCreateFilter)
+router.get('/checklistOnRejectegetting',protectRoute, checklistOnRejectegetting) 
+router.patch('/rejectChecklist',protectRoute, rejectChecklist)
+router.post('/checkListRejectedFilter',protectRoute, checkListRejectedFilter)
+/*checklist ends*/
+/* getting compliance for checklist select start*/
+router.get('/gettingchecklistAllCompliance',protectRoute, gettingchecklistAllCompliance)
 
-// var upload = multer({ storage: storage })
-router.post('/createCompliances', upload.fields([{ name: 'form' }, { name: 'docattachment' }]), createCompliances)
-router.post('/complianceFilter', complianceFilter)
-router.post('/complianceRejectedFilter', complianceRejectedFilter)
-router.post('/complianceApproveFilter', complianceApproveFilter)
-// router.get('/complianceGetting', complianceGetting )
-router.get('/gettingCompliances', gettingCompliances)
-router.put('/updateCompliancesById/:id', upload.fields([{ name: 'form' }, { name: 'docattachment' }]), updateCompliancesById)
+/* getting compliance for checklist select ends*/
+/*liseReg start*/ 
+router.post('/createLiseReg', upload.fields([{ name: 'licenseUpload' }, { name: 'challanUpload' }, { name: 'acknowledge' }, { name: 'documents' }]), protectRoute,createLiseReg)
+router.get('/liseRegGetting', protectRoute,liseRegGetting)
+router.get('/liseRegGettingById/:id', protectRoute,liseRegGettingById)
+router.put('/liseRegUpdateById/:id', upload.fields([{ name: 'licenseUpload' }, { name: 'challanUpload' }, { name: 'acknowledge' }, { name: 'documents' }]), protectRoute, liseRegUpdateById)
+router.post('/liseRegHistoryFilter', protectRoute, liseRegHistoryFilter)
+router.patch('/regsApporve', protectRoute, regsApporve)
+/*liseReg end*/
 
-// --------- User Route ----------
-router.post('/userCreate', userCreate)
-router.get('/userGetting', userGetting)
-
-// --------State Route : -----------
-router.post('/stateCreate', stateCreate)
-
-// -------------Checklist Route --------------
-router.post('/checkListCreate', upload.fields([{ name: 'image' }, { name: 'documents' }]), checkListCreate)
-router.get('/checkListGetting', checkListGetting)
-router.get('/checklistOnCreateegetting', checklistOnCreateegetting)
-router.get('/checklistOnRejectegetting', checklistOnRejectegetting)
-router.get('/checklistApprovegetting', checklistApprovegetting)
-// router.get('/checkListFilter/:state/:createdAt', checkListFilter)
-router.get('/checkListFilter', checkListFilter)
-router.get('/checkListFind', checkListFind)
-router.post('/checkListAllFilter', checkListAllFilter)
-router.post('/checkListApproveFilter', checkListApproveFilter)
-router.post('/checkListCreateFilter', checkListCreateFilter)
-router.post('/checkListRejectedFilter', checkListRejectedFilter)
-
-// --------------- Branch Route --------------
-router.post('/createBranch', createBranch)
-router.get('/branchGetting', branchGetting)
-
-// -------------------- Notification Route -----------
-router.post('/createNotification', createNotification)
-router.get('/notificationGetting', notificationGetting)
-
-// ------------------ Executive Route -----------
-
-// router.post('/createExecutive', createExecutive)
-// router.get('/executiveGetting', executiveGetting)
-
-router.post('/addlist', addlist)
-
-router.post('/checkData', checkData)
-router.get('/getCheckData', getCheckData)
-
-
-// --------- Elibrary Routes ------------------
-router.post('/createElibrary', upload.single('image'), createElibrary)
-router.get('/elibraryGetting', elibraryGetting)
-router.get('/elibraryApproved', elibraryApproved)
-router.get('/elibraryRejected', elibraryRejected)
-router.get('/elibraryGettingById/:id', elibraryGettingById)
-
-// --------------- Audit Routes --------------------
-router.get('/gettingAuditor', gettingAuditor)
-router.get('/gettingChecklist', gettingChecklist)
-
-router.post('/createAudit', createAudit)
-router.get('/auditGetting', auditGetting)
-router.put('/updateAudit/:id', updateAudit)
-router.post('/auditFilter', auditFilter)
-
-
-// ----------------- Lise & Reg Routes ------------
-router.post('/createLiseReg', upload.fields([{ name: 'licenseUpload' }, { name: 'challanUpload' }, { name: 'acknowledge' }, { name: 'documents' }]), createLiseReg)
-router.get('/liseRegGetting', liseRegGetting)
-router.put('/liseRegUpdateById/:id', liseRegUpdateById)
-router.post('/liseRegHistoryFilter', liseRegHistoryFilter)
-
-// -------------------- Company Create -------------------
-
-router.post('/createCompany',upload.any(), createCompany);
-router.get('/gettingCompany', gettingCompany);
-router.get('/gettingCompanyTable', gettingCompanyTable);
+/*checking which routes are called start*/
+router.use((req, res, next) => {
+    console.log(`In router: ${req.method}:${req.originalUrl}`);
+    next();
+});
+/*checking which routes are called ends*/
+/*company start*/
+router.post('/createCompany',upload.any(),protectRoute, createCompany);
+router.get('/gettingCompany',protectRoute, gettingCompany);
+router.get('/gettingCompanyTable',protectRoute, gettingCompanyTable);
+/*company end*/
+// router.post('/createCompany',  upload.any(), protectRoute,createCompany)
+// router.post('/companytab2',  upload.array('files'), protectRoute,createRegistrationTab2)
+// router.post('/companytab2',  upload.any(), protectRoute,createRegistrationTab2)
+// router.post('/companytab3',  upload.any(), protectRoute,createRegistrationTab3)
+// router.post('/companytab4',  upload.any(), protectRoute,createRegistrationTab4)
+// router.post('/companytab5',  upload.any(), protectRoute,createRegistrationTab5)
+// router.post('/companytab6',  upload.any(), protectRoute,createRegistrationTab6)
+// router.post('/companytab7', upload.any(), protectRoute,createRegistrationTab7)
 
 export default router;
